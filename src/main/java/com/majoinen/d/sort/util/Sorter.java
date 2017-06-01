0b50b04321cf22aa3600d81ee6db40688518ea6a
Daniel Majoinen <daniel@majoinen.com>
2017-06-01 22:52:08 +1000

    Add sort methods with `Comparator` property to `Sorter`

--- src/main/java/com/majoinen/d/sort/util/Sorter.java
+++ src/main/java/com/majoinen/d/sort/util/Sorter.java
@@ -1,5 +1,7 @@
 package com.majoinen.d.sort.util;
 
+import java.util.Comparator;
+
 /**
  * Sorter defines what sort methods are required for each sorting algorithm
  * implementation.
@@ -16,9 +18,25 @@ public interface Sorter<T> {
     void sort(SortableList<T> list);
 
     /**
+     * Sort the entire specified SortableList using the specified Comparator.
+     * @param list The SortableList to sort.
+     * @param comparator The Comparator to define the order.
+     */
+    void sort(SortableList<T> list, Comparator<T> comparator);
+
+    /**
      * Sort the specified SortableList for the specified amount of iterations.
      * @param iterations How many iterations to complete before stopping.
      * @param list The SortableList to sort.
      */
     void sort(int iterations, SortableList<T> list);
+
+    /**
+     * Sort the specified SortableList for the specified amount of iterations
+     * using the provided Comparator.
+     * @param iterations How many iterations to complete before stopping.
+     * @param list The SortableList to sort.
+     * @param comparator The Comparator to define the order.
+     */
+    void sort(int iterations, SortableList<T> list, Comparator<T> comparator);
 }
