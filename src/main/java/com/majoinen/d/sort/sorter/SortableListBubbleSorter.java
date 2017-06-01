b61742e13989f442039d534e9c8dae3962352fc0
Daniel Majoinen <daniel@majoinen.com>
2017-06-02 00:42:25 +1000

    Implement `SortableListBubbleSorter` `singleIteration` method

--- src/main/java/com/majoinen/d/sort/sorter/SortableListBubbleSorter.java
+++ src/main/java/com/majoinen/d/sort/sorter/SortableListBubbleSorter.java
@@ -1,17 +1,32 @@
 package com.majoinen.d.sort.sorter;
 
 import com.majoinen.d.sort.util.SortableList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.Collections;
 import java.util.Comparator;
 
 /**
  * <p>Sorter which uses Bubble Sort as its implementation.</p>
  *
- * <p></p>
+ * <p>1.First iteration, compare each adjacent pair of elements and swap them
+ * if they are out of order. Eventually the largest element gets propagated to
+ * the end.
+ * 2. Second iteration, repeat the process, but only from the first to the 2nd
+ * last element. Eventually second largest element is at the 2nd last element.
+ * 3. Repeat until all elements are sorted.</p>
+ *
  * @param <T> The type of objects to order.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 1/6/17
  */
 public class SortableListBubbleSorter<T extends Comparable<T>> extends
   AbstractSorter<T> {
+
+    private static final Logger logger =
+      LogManager.getLogger(SortableListBubbleSorter.class);
 
     public SortableListBubbleSorter() {
         // Empty constructor for when no default comparator is used
@@ -30,6 +45,18 @@ public class SortableListBubbleSorter<T extends Comparable<T>> extends
     @Override
     public void singleIteration(int iteration, SortableList<T> list,
       Comparator<T> comparator) {
-        // TODO: Implement bubble sort
+        for (int i = 0; i < list.size() - 1; i++) {
+            if(list.get(i).compareTo(list.get(i+1)) > 0) {
+                logger.info(String.format("Swapping %s with %s",
+                  list.get(i), list.get(i+1)));
+                Collections.swap(list, i, i+1);
+                list.print();
+            }
+            else {
+                logger.info(String.format("No need to swap %s and %s",
+                  list.get(i), list.get(i+1)));
+                list.print();
+            }
+        }
     }
 }
