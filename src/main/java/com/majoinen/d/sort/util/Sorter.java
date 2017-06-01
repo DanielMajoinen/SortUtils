ff02baadd0824596b0b0cf3f507938002806f980
Daniel Majoinen <daniel@majoinen.com>
2017-06-01 15:39:16 +1000

    Add `Sorter` interface which defines sort methods for sorting algorithms

---
+++ src/main/java/com/majoinen/d/sort/util/Sorter.java
@@ -0,0 +1,24 @@
+package com.majoinen.d.sort.util;
+
+/**
+ * Sorter defines what sort methods are required for each sorting algorithm
+ * implementation.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 1/6/17
+ */
+public interface Sorter {
+
+    /**
+     * Sort the entire specified SortableList.
+     * @param list The SortableList to sort.
+     */
+    void sort(SortableList list);
+
+    /**
+     * Sort the specified SortableList for the specified amount of iterations.
+     * @param iterations How many iterations to complete before stopping.
+     * @param list The SortableList to sort.
+     */
+    void sort(int iterations, SortableList list);
+}
