46acf90fb8eaac791ebeb6833dbe3eec91b68bf6
Daniel Majoinen <daniel@majoinen.com>
2017-06-01 15:42:04 +1000

    Add base `SortAlgorithm` enum for sorting algorithms

---
+++ src/main/java/com/majoinen/d/sort/util/SortAlgorithm.java
@@ -0,0 +1,11 @@
+package com.majoinen.d.sort.util;
+
+/**
+ * Enum for all sorting algorithms available.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 1/6/17
+ */
+public enum SortAlgorithm {
+    SELECTION, BUBBLE
+}
