f332bb0d408ae918fe04e053a0c9ecac6ade6491
Daniel Majoinen <daniel@majoinen.com>
2017-06-02 18:51:29 +1000

    Add `IntegerSortableArrayListETBubbleSortComparatorTest` unit test class

---
+++ src/test/java/com/majoinen/d/sort/util/IntegerSortableArrayListETBubbleSortComparatorTest.java
@@ -0,0 +1,43 @@
+package com.majoinen.d.sort.util;
+
+import com.majoinen.d.sort.sorter.SorterAlgorithm;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Sort a SortableArrayList using a comparator to define order.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 2/6/17
+ */
+public class IntegerSortableArrayListETBubbleSortComparatorTest
+  extends IntegerSortableListTest {
+
+    private SortableArrayList<Integer> list;
+    private static ReverseComparator<Integer> comparator;
+
+    @BeforeClass
+    public static void beforeClass() {
+        comparator = new ReverseComparator<>();
+    }
+
+    @Before
+    public void beforeEachTest() {
+        list = new SortableArrayList<>(INT_LIST_1_TO_5, comparator);
+    }
+
+    @Test
+    public void sortAll() {
+        list.sort(SorterAlgorithm.ET_BUBBLE);
+        assertTrue(assertAllTrue(list, 5,4,3,2,1));
+    }
+
+    @Test
+    public void sortAllComparatorOverride() {
+        list.sort(SorterAlgorithm.ET_BUBBLE, comparator.reversed());
+        assertTrue(assertAllTrue(list, 1,2,3,4,5));
+    }
+}
